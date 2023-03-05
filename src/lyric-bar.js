320867524a928015d28cd94a6aa61cb8d8dc85ae
solstice23 <solstice233@gmail.com>
2023-03-05 13:31:03 +0800

    Add many options & Fix bugs

--- src/lyric-bar.js
+++ src/lyric-bar.js
@@ -10,10 +10,72 @@ const useCallback = React.useCallback;
 const useRef = React.useRef;
 
 export function LyricBar(props) {
+	const [posX, setPosX] = useState(getSetting('posX', 'right'));
+	const [posY, setPosY] = useState(getSetting('posY', 'bottom'));
+	const [textAlign, setTextAlign] = useState(getSetting('text-align', 'left'));
+	const [opacity, setOpacity] = useState(getSetting('opacity', 100));
+	const [mouseThrough, setMouseThrough] = useState(!!getSetting('mouse-through', 'false'));
+
+	const innerRef = useRef();
+
+	useEffect(() => {
+		const onPosXChange = (e) => {
+			setPosX(e.detail ?? 'right');
+		}
+		const onPosYChange = (e) => {
+			setPosY(e.detail ?? 'bottom');
+		}
+		const onTextAlignChange = (e) => {
+			setTextAlign(e.detail ?? 'left');
+		}
+		const onOpacityChange = (e) => {
+			setOpacity(e.detail ?? 100);
+		}
+		const onMouseThroughChange = (e) => {
+			setMouseThrough(e.detail ?? false);
+		}
+		document.addEventListener("lb-posX", onPosXChange);
+		document.addEventListener("lb-posY", onPosYChange);
+		document.addEventListener("lb-text-align", onTextAlignChange);
+		document.addEventListener("lb-opacity", onOpacityChange);
+		document.addEventListener("lb-mouse-through", onMouseThroughChange);
+		return () => {
+			document.removeEventListener("lb-posX", onPosXChange);
+			document.removeEventListener("lb-posY", onPosYChange);
+			document.removeEventListener("lb-text-align", onTextAlignChange);
+			document.removeEventListener("lb-opacity", onOpacityChange);
+			document.removeEventListener("lb-mouse-through", onMouseThroughChange);
+		}
+	}, []);
+
+	useEffect(() => {
+		innerRef.current.parentElement.classList.toggle('posx-left', posX == 'left');
+		innerRef.current.parentElement.classList.toggle('posx-right', posX == 'right');
+	}, [posX]);
+
+	useEffect(() => {
+		innerRef.current.parentElement.classList.toggle('posy-top', posY == 'top');
+		innerRef.current.parentElement.classList.toggle('posy-bottom', posY == 'bottom');
+	}, [posY]);
+
+	useEffect(() => {
+		innerRef.current.parentElement.classList.toggle('text-align-left', textAlign == 'left');
+		innerRef.current.parentElement.classList.toggle('text-align-center', textAlign == 'center');
+		innerRef.current.parentElement.classList.toggle('text-align-right', textAlign == 'right');
+	}, [textAlign]);
+
+	useEffect(() => {
+		innerRef.current.parentElement.style.opacity = opacity / 100;
+	}, [opacity]);
+
+	useEffect(() => {
+		innerRef.current.parentElement.classList.toggle('mouse-through', mouseThrough);
+	}, [mouseThrough]);
+
 	return (
 		<>
-			<Resizer />
-			<div className='lyric-bar-inner'>
+			<Resizer posX={posX} />
+			<div className={`lyric-bar-inner`} ref={innerRef}>
 				<Lyrics />
 			</div>
 		</>
@@ -25,9 +87,17 @@ function Resizer(props) {
 	
 	useEffect(() => {
 		const onMouseMove = (e) => {
-			const clientX = e.clientX;
-			const containerRight = ref.current.parentElement.getBoundingClientRect().right;
-			const width = Math.max(containerRight - clientX, 200);
+			let width;
+			if (props.posX == 'right') {
+				const clientX = e.clientX;
+				const containerRight = ref.current.parentElement.getBoundingClientRect().right;
+				width = Math.max(containerRight - clientX, 200);
+			} else if (props.posX == 'left') {
+				const clientX = e.clientX;
+				const containerLeft = ref.current.parentElement.getBoundingClientRect().left;
+				width = Math.max(clientX - containerLeft, 200);
+			}
+			
 			ref.current.parentElement.style.setProperty('--lyric-bar-width', `${width}px`);
 			ref.current.parentElement.style.setProperty('--mask-image', `linear-gradient(90deg,
 				rgba(0,0,0,0) 0%,
@@ -58,7 +128,7 @@ function Resizer(props) {
 		return () => {
 			ref.current.removeEventListener('mousedown', onMouseDown);
 		};
-	}, []);
+	}, [props.posX]);
 
 	useEffect(() => {
 		ref.current.parentElement.style.setProperty('--lyric-bar-width', getSetting('lyric-bar-width', '400px'));
@@ -76,6 +146,9 @@ function Resizer(props) {
 		<div
 			className='lyric-bar-resizer'
 			ref={ref}
+			style={{
+				[props.posX == 'right' ? 'left' : 'right']: 0,
+			}}
 		/>
 	);
 }
--- src/settings.js
+++ src/settings.js
@@ -8,6 +8,13 @@ import FormGroup from '@mui/material/FormGroup';
 import Switch from '@mui/material/Switch';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Stack from '@mui/material/Stack';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const useState = React.useState;
@@ -17,57 +24,125 @@ const useMemo = React.useMemo;
 const useCallback = React.useCallback;
 const useRef = React.useRef;
 
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+const lightTheme = createTheme({
+	palette: {
+		mode: 'light',
+	},
+});
 
 export function Settings(props) {
 	return (
-		<div>
-			<Typography gutterBottom>提示：Lyric Bar 现处于早期版本，可能会出现各种问题，欢迎反馈</Typography>
-			<FormGroup>
-				<FormControlLabel control={
-					<Switch defaultChecked={!!getSetting('adaptive-width', 'true')} onChange={(e) => {
-						document.dispatchEvent(new CustomEvent('lb-adaptive-width', { detail: e.target.checked }));
-						setSetting('adaptive-width', e.target.checked);
-					}} />
-				} label="自适应宽度" />
-				<FormControlLabel control={
-					<Switch defaultChecked={!!getSetting('show-translation', 'true')} onChange={(e) => {
-						document.dispatchEvent(new CustomEvent('lb-show-translation', { detail: e.target.checked }));
-						setSetting('show-translation', e.target.checked);
-					}} />
-				} label="显示翻译" />
-				<FormControlLabel control={
-					<Switch defaultChecked={!!getSetting('show-romaji', 'false')} onChange={(e) => {
-						document.dispatchEvent(new CustomEvent('lb-show-romaji', { detail: e.target.checked }));
-						setSetting('show-romaji', e.target.checked);
-					}} />
-				} label="显示罗马音" />
-				<FormControlLabel control={
-					<Switch defaultChecked={!!getSetting('use-karaoke-lyrics', 'true')} onChange={(e) => {
-						document.dispatchEvent(new CustomEvent('lb-use-karaoke-lyrics', { detail: e.target.checked }));
-						setSetting('use-karaoke-lyrics', e.target.checked);
-					}} />
-				} label="逐字歌词" />
-			</FormGroup>
-			<Typography gutterBottom>字体大小</Typography>
-			<Slider
-				defaultValue={parseInt(getSetting('lyric-font-size', 20))}
-				valueLabelDisplay="auto"
-				min={10}
-				max={50}
-				onChange={(e, value) => {
-					document.dispatchEvent(new CustomEvent('lb-lyric-font-size', { detail: value }));
-					setSetting('lyric-font-size', value);
-				}}
-				marks={[
-					{ value: 20 }
-				]}
-			/>
-			<FormControlLabel control={
-				<Switch defaultChecked={!!getSetting('first-line-bold', 'false')} onChange={(e) => {
-					document.dispatchEvent(new CustomEvent('lb-first-line-bold', { detail: e.target.checked }));
-					setSetting('first-line-bold', e.target.checked);
-				}} />
-			} label="加粗原文" />
-		</div>
+		<ThemeProvider theme={darkTheme}>
+			<div className='lyric-bar-settings' style={{backgroundColor: '#0a1929', padding: '15px', borderRadius: '10px'}}>
+				<Stack direction="column" spacing={2}>
+					<Typography gutterBottom>提示：Lyric Bar 现处于早期版本，可能会出现各种问题，欢迎反馈</Typography>
+					<FormGroup>
+						<FormControlLabel control={
+							<Switch defaultChecked={!!getSetting('adaptive-width', 'true')} onChange={(e) => {
+								document.dispatchEvent(new CustomEvent('lb-adaptive-width', { detail: e.target.checked }));
+								setSetting('adaptive-width', e.target.checked);
+							}} />
+						} label="自适应宽度" />
+						
+						<Stack direction="row" spacing={2} alignItems="flex-end">
+							<FormControl style={{ width: 'fit-content' }}>
+								<FormLabel>水平位置</FormLabel>
+								<RadioGroup	row defaultValue={getSetting('posX', 'right')} onChange={(e) => {
+									document.dispatchEvent(new CustomEvent('lb-posX', { detail: e.target.value }));
+									setSetting('posX', e.target.value);
+								}}>
+									<FormControlLabel value="left" control={<Radio />} label="居左" />
+									<FormControlLabel value="right" control={<Radio />} label="居右" />
+								</RadioGroup>
+							</FormControl>
+
+							<FormControl style={{ width: 'fit-content' }}>
+								<FormLabel>垂直位置</FormLabel>
+								<RadioGroup	row defaultValue={getSetting('posY', 'bottom')} onChange={(e) => {
+									document.dispatchEvent(new CustomEvent('lb-posY', { detail: e.target.value }));
+									setSetting('posY', e.target.value);
+								}}>
+									<FormControlLabel value="top" control={<Radio />} label="居上" />
+									<FormControlLabel value="bottom" control={<Radio />} label="居下" />
+								</RadioGroup>
+							</FormControl>
+						</Stack>
+
+						<FormControlLabel control={
+							<Switch defaultChecked={!!getSetting('mouse-through', 'false')} onChange={(e) => {
+								document.dispatchEvent(new CustomEvent('lb-mouse-through', { detail: e.target.checked }));
+								setSetting('mouse-through', e.target.checked);
+							}} />
+						} label="鼠标穿透" />
+						<Typography gutterBottom>不透明度</Typography>
+						<Slider
+							defaultValue={parseInt(getSetting('opacity', 100))}
+							valueLabelDisplay="auto"
+							min={5}
+							max={100}
+							onChange={(e, value) => {
+								document.dispatchEvent(new CustomEvent('lb-opacity', { detail: value }));
+								setSetting('opacity', value);
+							}}
+						/>
+
+						<FormControlLabel control={
+							<Switch defaultChecked={!!getSetting('show-translation', 'true')} onChange={(e) => {
+								document.dispatchEvent(new CustomEvent('lb-show-translation', { detail: e.target.checked }));
+								setSetting('show-translation', e.target.checked);
+							}} />
+						} label="显示翻译" />
+						<FormControlLabel control={
+							<Switch defaultChecked={!!getSetting('show-romaji', 'false')} onChange={(e) => {
+								document.dispatchEvent(new CustomEvent('lb-show-romaji', { detail: e.target.checked }));
+								setSetting('show-romaji', e.target.checked);
+							}} />
+						} label="显示罗马音" />
+						<FormControlLabel control={
+							<Switch defaultChecked={!!getSetting('use-karaoke-lyrics', 'true')} onChange={(e) => {
+								document.dispatchEvent(new CustomEvent('lb-use-karaoke-lyrics', { detail: e.target.checked }));
+								setSetting('use-karaoke-lyrics', e.target.checked);
+							}} />
+						} label="逐字歌词" />
+					</FormGroup>
+					<Typography gutterBottom>字体大小</Typography>
+					<Slider
+						defaultValue={parseInt(getSetting('lyric-font-size', 20))}
+						valueLabelDisplay="auto"
+						min={10}
+						max={50}
+						onChange={(e, value) => {
+							document.dispatchEvent(new CustomEvent('lb-lyric-font-size', { detail: value }));
+							setSetting('lyric-font-size', value);
+						}}
+						marks={[
+							{ value: 20 }
+						]}
+					/>
+					<FormControl>
+						<FormLabel>文本对齐</FormLabel>
+						<RadioGroup	row defaultValue={getSetting('text-align', 'center')} onChange={(e) => {
+							document.dispatchEvent(new CustomEvent('lb-text-align', { detail: e.target.value }));
+							setSetting('text-align', e.target.value);
+						}}>
+							<FormControlLabel value="left" control={<Radio />} label="居左" />
+							<FormControlLabel value="center" control={<Radio />} label="居中" />
+							<FormControlLabel value="right" control={<Radio />} label="居右" />
+						</RadioGroup>
+					</FormControl>
+					<FormControlLabel control={
+						<Switch defaultChecked={!!getSetting('first-line-bold', 'false')} onChange={(e) => {
+							document.dispatchEvent(new CustomEvent('lb-first-line-bold', { detail: e.target.checked }));
+							setSetting('first-line-bold', e.target.checked);
+						}} />
+					} label="加粗原文" />
+				</Stack>
+			</div>
+		</ThemeProvider>
 	);
 }
